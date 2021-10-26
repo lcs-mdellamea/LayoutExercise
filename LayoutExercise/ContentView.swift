8ef7355c64013a2a81c47dbe79326313e242f968
Madison Dellamea <mdellamea@lcs.on.ca>
2021-10-26 14:16:34 -0400

    Initial Commit

---
+++ LayoutExercise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LayoutExercise
+//
+//  Created by Madison Dellamea on 10/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LayoutExercise/LayoutExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutExerciseApp.swift
+//  LayoutExercise
+//
+//  Created by Madison Dellamea on 10/26/21.
+//
+
+import SwiftUI
+
+@main
+struct LayoutExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
